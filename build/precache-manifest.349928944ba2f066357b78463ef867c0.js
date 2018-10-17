3760d2c1c31694ad574af20cc5ba5c682c5bb702
fisheep8 <fisheep8@gmail.com>
2018-10-18 00:33:30 +1300

    build

---
+++ build/precache-manifest.349928944ba2f066357b78463ef867c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6920012764997a38f0cf",
+    "url": "/static/js/main.69200127.chunk.js"
+  },
+  {
+    "revision": "dd32000a6ce7d73f3e5f",
+    "url": "/static/js/1.dd32000a.chunk.js"
+  },
+  {
+    "revision": "6920012764997a38f0cf",
+    "url": "/static/css/main.22f3bc08.chunk.css"
+  },
+  {
+    "revision": "1c42a44e0405762b47716f7f65ec6f81",
+    "url": "/index.html"
+  }
+];
